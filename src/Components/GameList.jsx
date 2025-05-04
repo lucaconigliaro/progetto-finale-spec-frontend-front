137c44c5b16e6b64b5dc3b53901fffd42135cfc6
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-05-04 20:41:30 +0200

    implement search functionality in HomePage

--- src/Components/GameList.jsx
+++ src/Components/GameList.jsx
@@ -1,19 +1,15 @@
-import { useContext } from "react";
-import { GlobalContext } from "../Context/GlobalContext";
 import GameCard from "./GameCard";
 
-export default function GamesList() {
-    const { games } = useContext(GlobalContext);
-
-    return (
-        <div className="container mt-4">
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-                {games.map((game) => (
-                    <div key={game.id} className="col">
-                        <GameCard game={game} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+export default function GamesList({ games }) {
+  return (
+    <div className="container mt-4">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        {games.map((game) => (
+          <div key={game.id} className="col">
+            <GameCard game={game} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,31 @@
-import GamesList from "../Components/GameList";
+import { useContext, useState } from "react";
+import GameList from "../Components/GameList";
+import { GlobalContext } from "../Context/GlobalContext";
 
 export default function HomePage() {
+    const { games } = useContext(GlobalContext);
+    const [searchGame, setSearchGame] = useState("");
+
+    const handleChange = (e) => {
+        setSearchGame(e.target.value);
+    };
+
+    const filteredGames = games.filter(game =>
+        game.title.toLowerCase().includes(searchGame.toLowerCase())
+    );
+
     return (
-        <div>
+        <div className="container mt-4">
+            <input
+                type="text"
+                placeholder="Cerca un gioco..."
+                onChange={handleChange}
+                value={searchGame}
+                className="form-control mb-4"
+            />
+
             <h1>I nostri giochi</h1>
-            <GamesList />
+            <GameList games={filteredGames} />
         </div>
     );
-};
+}
